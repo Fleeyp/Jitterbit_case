ca9a36af25c58b0e2a9cc6132bfe9d34a4116134
fleeyp <fleeyp@gmail.com>
2026-03-07 16:26:41 -0300

    creating the first files

---
+++ config/database.js
@@ -0,0 +1,36 @@
+const sqlite3 = require("sqlite3").verbose();
+
+const db = new sqlite3.Database("./orders.db", (err) => {
+    if (err) {
+        console.error("Erro ao conectar no banco", err);
+    } else {
+        console.log("Banco conectado.");
+    }
+});
+
+db.serialize(() => {
+
+    db.run(`
+        CREATE TABLE IF NOT EXISTS "Order" (
+            orderId TEXT PRIMARY KEY,
+            value REAL,
+            creationDate TEXT
+        )
+    `);
+
+    db.run(`
+        CREATE TABLE IF NOT EXISTS Items (
+            orderId TEXT,
+            productId INTEGER,
+            quantity INTEGER,
+            price REAL,
+            FOREIGN KEY(orderId) REFERENCES "Order"(orderId)
+        )
+    `);
+
+});
+
+export default db;
+
+// Na criação da tabela order, precisei colocar entre aspas o nome da tabela,
+// pois 'order' é uma palavra reservada SQL
